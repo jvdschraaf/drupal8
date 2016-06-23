1edac73e6ecad9bd535c6e0cf37057bf3de5c1f2
Jeroen van de Schraaf <jvdschraaf@ibuildings.nl>
2016-06-23 12:45:50 +0200

    Update test, add gitignore

--- tests/smoke-test.sh
+++ tests/smoke-test.sh
@@ -7,7 +7,18 @@ while ! TEST_OUTPUT=`curl -s --fail http://localhost`;
 done
 
 ## Assert server response
-if [ "$TEST_OUTPUT" != "Hello!" ]
+if [ "$TEST_OUTPUT" != '<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="refresh" content="1;url=/core/install.php" />
+
+        <title>Redirecting to /core/install.php</title>
+    </head>
+    <body>
+        Redirecting to <a href="/core/install.php">/core/install.php</a>.
+    </body>
+</html>' ]
 then
-    echo "Failed asserting that '${TEST_OUTPUT}' equals 'Hello!'" && exit 1;
+    echo "Failed asserting that '${TEST_OUTPUT}' equals Drupal Installer response" && exit 1;
 fi
